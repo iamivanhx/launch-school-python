aea0898783ec8833c69a6d7054b358617e37dacc
Ivan E. Hernandez T. <ivan@ivanhx.com>
2025-12-15 20:56:32 +0100

    Data Types - Ex3

---
+++ PY100/1.data_types/ex3.py
@@ -0,0 +1,8 @@
+# Create a dictionary named pets that contains a list of pet names and the type of animal. 
+pets = {
+    'Asta': 'dog',
+    'Butterscotch': 'cat',
+    'Pudding': 'cat',
+    'Neptune': 'fish',
+    'Darwin': 'lizard',
+}
