ff376293629d5dc0ee3dea5d25a383133966c480
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-01-12 19:17:13 +0100

    Basic Operations: Exercise 8

--- PY100/2.basic_operations/ex8.py
+++
@@ -1,2 +0,0 @@
-# To what value does the following expression evaluate?
-'12' < '9' # => True
---
+++ PY100/2.basic_operations/exercise_8.py
@@ -0,0 +1,4 @@
+# To what value does the following expression evaluate?
+'12' < '9'
+
+# Evaluates to True. Strings comparison are done lexicographically, meaning, character-by-character. SO when comparing the string '1' to the string '9' the result is True.
