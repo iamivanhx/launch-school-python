3f661835709ebd1b39e862dd2acda45c355befe9
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-01-12 19:11:48 +0100

    Basic Operations: Exercise 5

--- PY100/2.basic_operations/exercise_5.py
+++ PY100/2.basic_operations/exercise_5.py
@@ -1,6 +1,5 @@
 # Will an error occur if you try to access a list element with an index greater than or equal to the list's length? For example:
-
 foo = ['a', 'b', 'c']
 print(foo[3])      # will this result in an error?
 
-# Yes, an index out of range exception will be raised.
+# Yes. It will raise an exception index out of range.
