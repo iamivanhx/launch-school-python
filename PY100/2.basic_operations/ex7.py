141638ccc21f1aed04fd0c8b06b694eb9a9dab42
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-01-12 19:14:52 +0100

    Basic Operations: Exercise 7

--- PY100/2.basic_operations/ex7.py
+++
@@ -1,2 +0,0 @@
-# What will the following code do? Why?
-int('3.1415') # => Raise a ValueError exception as the String does not represent a valid integer
---
+++ PY100/2.basic_operations/exercise_7.py
@@ -0,0 +1,4 @@
+# What will the following code do? Why?
+int('3.1415')
+
+# It will raise a ValueError exception as the string does not represent an integer value.
