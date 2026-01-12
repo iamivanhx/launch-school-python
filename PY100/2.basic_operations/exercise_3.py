17d6392381647e380beb4bd2716b80218d0b3f6d
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-01-12 19:08:08 +0100

    Basic Operations: Exercise 3

---
+++ PY100/2.basic_operations/exercise_3.py
@@ -0,0 +1,4 @@
+# What does the following code do? Why?
+print('5' + '10')
+
+# It outputs the string object with value 510. Reason is that the `+` operator when use with string types, concatenate the operands.
