3992db083728fc4f451c46a112267e627239c260
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-01-12 18:55:46 +0100

    Basic Operations: Exercise 1

--- PY100/2.basic_operations/exercise_1.py
+++ PY100/2.basic_operations/exercise_1.py
@@ -1,6 +1,9 @@
 # Use concatenation to create your full name from your first and last names. The new string should contain your first name, a space, and your last name. For example, if your name is John Doe, you should combine 'John' and 'Doe' to get 'John Doe'.
 
-print('John' + ' ' + "Doe")
+full_name = 'John' + ' ' + 'Doe'
+print(full_name)
+
 first_name = 'John'
 last_name = 'Doe'
-print(first_name + ' ' + last_name)
+full_name = f"{first_name} {last_name}"
+print(full_name)
