90651f8d361b1f63209783b75a0c5a8ac259b94b
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-01-13 18:50:19 +0100

    Variables: Exercise 2

---
+++ PY100/3.variables/exercise_2.py
@@ -0,0 +1,10 @@
+# Classify the following potential function names as idiomatic, non-idiomatic, or illegal. For the non-idiomatic and illegal names, explain your choice.
+
+# index             # idiomatic
+# CatName           # non-idiomatic (CamelCase)
+# lazy_dog          # idiomatic
+# quick_Fox         # non-idiomatic (Uppercase letter)
+# 1stCharacter      # illegal (starts with a number)
+# operand2          # idiomatic
+# BIG_NUMBER        # non-idiomatic (All Caps)
+# π                 # non-idiomatic (non ASCII character)
