8cb21e1ebb520b091e52929eb273b4f4e292d6a6
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-01-13 18:52:35 +0100

    Variables: Exercise 3

---
+++ PY100/3.variables/exercise_3.py
@@ -0,0 +1,10 @@
+# Classify the following potential constant names as idiomatic, non-idiomatic, or illegal. For the non-idiomatic and illegal names, explain your choice.
+
+# index             # non-idiomatic (lower case word)
+# CatName           # non-idiomatic (CamelCase)
+# snake_case        # non-idiomatic 
+# LAZY_DOG3         # idiomatic
+# 1ST               # illegal (starts with a number)
+# operand2          # non-idiomatic (lower case)
+# BIG_NUMBER        # idiomatic
+# Π                 # non-idiomatic (Non ASCII Character)
