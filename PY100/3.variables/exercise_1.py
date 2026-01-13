2e0a258bb21195af4219911822595266c454a654
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-01-13 08:35:05 +0100

    Create exercise_1.py
    Variables: Exercise 1

---
+++ PY100/3.variables/exercise_1.py
@@ -0,0 +1,10 @@
+# Classify the following potential non-constant variable names as idiomatic, non-idiomatic, or illegal. For the non-idiomatic and illegal names, explain your choice.
+
+# index             # idiomatic
+# CatName           # non-idiomatic: CamelCase only for classes
+# lazy_dog          # idiomatic
+# quick_Fox         # non-idiomatic: Uppercased letter second word
+# 1stCharacter      # illegal: start with a number
+# operand2          # idiomatic
+# BIG_NUMBER        # non-idiomatic: Uppercase only for constants
+# π                 # non-idiomatic: is not an ASCII Symbol
