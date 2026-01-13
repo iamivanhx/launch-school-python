e19c5adf77a3205b86a45af30a040a00ed8e9bc6
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-01-13 19:13:27 +0100

    Variables: Exercise 10

---
+++ PY100/3.variables/exercise_10.py
@@ -0,0 +1,11 @@
+# Assume that obj already has a value of 42 when the code below starts running. Which of the subsequent statements reassign the variable? Which statements mutate the value of the object that obj references? Which statements do neither?
+obj = 'ABcd'            # reasignment
+obj.upper()             # neither
+obj = obj.lower()       # reassignment
+print(len(obj))         # neither
+obj = list(obj)         # reassignment
+obj.pop()               # mutation
+obj[2] = 'X'            # mutation
+obj.sort()              # mutation
+set(obj)                # neither
+obj = tuple(obj)        # reassignment    
