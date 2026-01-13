a3c2144f400c8e6c8a79b85fb6c47200f3efd8fc
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-01-13 18:55:33 +0100

    Variables: Exercise 4

---
+++ PY100/3.variables/exercise_4.py
@@ -0,0 +1,9 @@
+# Classify the following potential class names as idiomatic, non-idiomatic, or illegal. For the non-idiomatic and illegal names, explain your choice.
+
+# index             # non-idiomatic (lowercase)
+# CatName           # idiomatic
+# Lazy_Dog          # non-idiomatic (separated by underscore)
+# 1ST               # illegal (starts with a number)
+# operand2          # non-idiomatic (lowercase)
+# BigNumber3        # idiomatic
+# Πi                # non-idiomatic (non ASCII character)
