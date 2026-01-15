a4f0bdb6ae4d19d221d8205f5ed70179ef443fba
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-01-15 08:39:08 +0100

    Create exercise_4.py
    Exercise 4

---
+++ PY100/5.functions_and_methods/exercise_4.py
@@ -0,0 +1,16 @@
+# Identify the following items in that code:
+
+def multiply_numbers(num1, num2, num3):
+    result = num1 * num2 * num3
+    return result
+
+product = multiply_numbers(2, 3, 4)
+
+# function name             # multiply_numbers
+# function arguments        # 2, 3, 4
+# function definition       # from lines 3 to 5
+# function body             # lines 4 and 5
+# function parameters       # num1, num2, num3
+# function invocation       # multiply_numbers(2, 3, 4)
+# function return value     # 24
+# all identifiers           # multiply_numbers, num1, num2, num3, result,     product, 2, 3, 4
