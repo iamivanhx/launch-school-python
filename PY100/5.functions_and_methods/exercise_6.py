8175f340764a96f374d74c67fdedd96fdac71e74
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-01-15 08:42:50 +0100

    Create exercise_6.py
    Exercise 6

---
+++ PY100/5.functions_and_methods/exercise_6.py
@@ -0,0 +1,7 @@
+# What does the following code print?
+def scream(words):
+    words = words + '!!!!'
+    return
+    print(words)
+
+scream('Yipeee') # There is no output as on line 4 there is a return statement that returns the value NoNe and the function execution stops and the lkine 5 is not executed.
