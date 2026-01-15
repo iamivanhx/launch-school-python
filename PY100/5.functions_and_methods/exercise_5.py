8a2f6e6c6a581e90d1c21b4e3e1dbb4888a2d1fa
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-01-15 08:40:33 +0100

    Create exercise_5.py
    Exercise 5

---
+++ PY100/5.functions_and_methods/exercise_5.py
@@ -0,0 +1,6 @@
+# What does the following code print?
+def scream(words):
+    return words + '!!!!'
+
+scream('Yipeee') # Nothing is printed out as the return value from the function invocation is not stored, so is lost.
+
