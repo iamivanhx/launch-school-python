41c6522ff50395cb1d0ece6d63d47cb691c1ccbf
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-01-15 08:44:45 +0100

    Create exercise_7.py
    Exercise 7

---
+++ PY100/5.functions_and_methods/exercise_7.py
@@ -0,0 +1,7 @@
+# Without running the following code, what do you think it will do?
+
+def foo(bar, qux):
+    print(bar)
+    print(qux)
+
+foo('Hello') # It raises an exception as the function is invoked with only 1 argument and two are expected, as the function is defined with two parameters.
