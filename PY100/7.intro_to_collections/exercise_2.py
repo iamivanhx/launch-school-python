20034cf52cbdd8b035b914eaf6c43b116ce6d78a
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-01-19 15:30:35 +0100

    Create exercise_2.py
    Exercise 2

---
+++ PY100/7.intro_to_collections/exercise_2.py
@@ -0,0 +1,5 @@
+# Can you write some code to change the value 'bye' in the following tuple to 'goodbye'?
+stuff = ('hello', 'world', 'bye', 'now')
+stuff = ('hello', 'world', 'goodbye', 'now')
+
+# Tuples are immutable. The only option is to reassign the tuple.
