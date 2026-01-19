5fb87ebcd446457dff16bcfe208df564094ff5c5
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-01-19 15:44:31 +0100

    Create exercise_9.py
    Exercise 9

---
+++ PY100/7.intro_to_collections/exercise_9.py
@@ -0,0 +1,15 @@
+# answer the following questions and explain your answers:
+my_list = [1, 2, 3, [4, 5, 6]]
+another_list = list(my_list)
+
+# Are the lists assigned to my_list and another_list equal?
+## Yes, they are equal as both have the same values
+
+# Are the lists assigned to my_list and another_list the same object?
+## No, they are different objects
+
+# Are the nested lists at index 3 of my_list and another_list equal?
+## Yes, they are equal
+
+# Are the nested lists at index 3 of my_list and another_list the same object?
+## Yes, they are the same object.
