63f7a04006e3db0ced03b35c2d3e0b7c5fc6304e
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-01-19 15:32:45 +0100

    Create exercise_3.py
    Exercise 3

---
+++ PY100/7.intro_to_collections/exercise_3.py
@@ -0,0 +1,9 @@
+# Identify at least 2 differences between lists and tuples. Identify at least 2 ways that lists and tuples are similar.
+
+# Differences
+# - lists are mutable; tuples are immutable
+# - lists literal is []; tuples literal is ()
+
+# Similarities
+# - Both are sequences
+# - Both are heterogenous 
