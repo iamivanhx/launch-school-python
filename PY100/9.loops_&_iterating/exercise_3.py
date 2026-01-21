d9b80704ed7e0bff1d771d6138ec07aa32602cab
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-01-21 08:36:32 +0100

    Create exercise_3.py
    Exercise 3

---
+++ PY100/9.loops_&_iterating/exercise_3.py
@@ -0,0 +1,16 @@
+# Use a while loop to print the numbers in my_list, one number per line. Then, do the same with a for loop.
+
+my_list = [6, 3, 0, 11, 20, 4, 17]
+
+print("While Loop")
+counter = 0
+
+while counter < len(my_list):
+    print(my_list[counter])
+    counter += 1
+
+print()
+print("For Loop")
+
+for num in my_list:
+    print(num)
