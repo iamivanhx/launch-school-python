42e2a664fe2825814d2c8f0d4d240750a4768e0a
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-01-28 18:59:54 +0100

    Variables as Pointers - Exercise 1

---
+++ PY100/10.variables_as_pointers/exercise_1.py
@@ -0,0 +1,6 @@
+# explain the difference between these two expressions.
+my_object1 == my_object2
+my_object1 is my_object2
+
+# Line 2 compares objects for equality
+# Line 3 checks if both variables point to the same object
