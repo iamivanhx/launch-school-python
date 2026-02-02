9d9e3e5319b8b9eba44d18718c05385e999a5b6e
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-02-02 19:08:58 +0100

    Python Basics - Documentation 1 - Checking data types

---
+++ PY100/python_basics/1.documentation1/9.checking_data_types.py
@@ -0,0 +1,5 @@
+# how would you identify the data type of the following values?
+values = [23.5, 'Call me Ishmael.', False, 0, None]
+
+for value in values:
+    print(f"Value: {value} is of type {type(value)}")
