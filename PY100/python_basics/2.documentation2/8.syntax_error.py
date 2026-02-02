8af3ac29909c6979a44784a3e45191f3894a67be
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-02-02 19:35:38 +0100

    Python Basics - Documentation 2 - Syntax error

---
+++ PY100/python_basics/2.documentation2/8.syntax_error.py
@@ -0,0 +1,18 @@
+# What does a SyntaxError indicate? Try running the above code, then use the resulting error message to fix the error.
+# speed_limit = 60
+# current_speed = 80
+
+# if current_speed > speed_limit
+#     print('"People are so bad at driving cars that '
+#           "computers don\'t have to be that good to be "
+#           'much better." -- Marc Andreessen')
+    
+# A syntax error indicates that an invalid python code was written that the interpreter cannot understand
+
+speed_limit = 60
+current_speed = 80
+
+if current_speed > speed_limit:
+    print('"People are so bad at driving cars that '
+          "computers don\'t have to be that good to be "
+          'much better." -- Marc Andreessen')
