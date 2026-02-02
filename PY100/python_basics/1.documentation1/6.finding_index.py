09747788ad080dc001cd30e3844774ba077c78aa
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-02-02 18:59:06 +0100

    Python Basics - Documentation 1 - Finding Index

---
+++ PY100/python_basics/1.documentation1/6.finding_index.py
@@ -0,0 +1,5 @@
+# How would you determine the index of the fruit "cherry" in this list?
+fruits = ["apple", "banana", "cherry", "peach", "watermelon"]
+
+if 'cherry' in fruits:
+    print(fruits.index('cherry'))
