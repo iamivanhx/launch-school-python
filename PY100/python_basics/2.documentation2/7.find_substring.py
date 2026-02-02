765e6ef7aa0a2ff04ac79df3e0e5e90cf9b6725f
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-02-02 19:33:14 +0100

    Python Basics - Documentation 2 - Find Substring

---
+++ PY100/python_basics/2.documentation2/7.find_substring.py
@@ -0,0 +1,6 @@
+# Using the official Python documentation, can you determine how to check whether a string contains a specific substring?
+
+my_string = 'Aleluya'
+sub_string = 'uya'
+
+print(sub_string in my_string)
