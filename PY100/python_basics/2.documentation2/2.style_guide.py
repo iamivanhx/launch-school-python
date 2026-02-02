031ffdd1f84130b0da251773bd84fb24a7401f6f
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-02-02 19:19:25 +0100

    Python Basics - Documentation 2 - Style Guide

---
+++ PY100/python_basics/2.documentation2/2.style_guide.py
@@ -0,0 +1,16 @@
+# In the following code snippet, find all violations of the PEP8 style guide. Rewrite it so that it conforms with the guide.
+# iceCreamDensity=10                # Use of CamelCase foe variables
+
+# while iceCreamDensity>0 :         # no separations between operand and operator
+#     print('Drip...')
+#     iceCreamDensity-=1
+
+# print('The ice cream melted.')
+
+ice_cream_dentsity = 10
+
+while ice_cream_dentsity > 0:
+    print('Drip...')
+    ice_cream_dentsity -= 1
+
+print('The ice cream melted.')
