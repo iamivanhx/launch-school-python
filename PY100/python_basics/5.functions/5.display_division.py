ac4c2ca3f3d619746a94ae452d6fc3ceba181d65
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-02-04 20:15:03 +0100

    Python Basics - Functions - Display Division

---
+++ PY100/python_basics/5.functions/5.display_division.py
@@ -0,0 +1,11 @@
+# Without running the following code, determine what it will print:
+def multiples_of_three():
+    divisor = 1
+
+    for dividend in range(3, 31, 3):
+        print(f'{dividend} / {divisor} = 3')
+        divisor += 1
+
+multiples_of_three
+
+# Prints nothing as the function is not invoked as it misses the parenthesis.
