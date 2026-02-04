d0c09b1082c831fec28e5ce02831d50f3b1dfef2
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-02-04 18:40:40 +0100

    Python Basics - Loops - And on

---
+++ PY100/python_basics/3.loops/8.and_on.py
@@ -0,0 +1,6 @@
+# The following code keeps looping forever. (You can hit Ctrl-C to stop it.) Why does the loop keep running? Modify it so that it stops after the first iteration.
+while True:
+    print("and on")
+    break
+
+# True is truthy so it keeps running forever. The break statement breaks out of the loop
