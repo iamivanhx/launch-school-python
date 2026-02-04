dcb033593690433ee31087c5cc00bf66e01269fc
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-02-04 20:20:41 +0100

    Python Basics - Functions - Transformation

---
+++ PY100/python_basics/5.functions/7.transformation.py
@@ -0,0 +1,5 @@
+# Use Python's string methods on the string 'Captain Ruby' to create a string with the value 'Captain Python'.
+string1 = "Captain Ruby"
+string2 = string1.replace("Ruby", "Python")
+
+print(string2)
