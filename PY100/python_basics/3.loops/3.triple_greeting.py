fb0df65c035c9472db21ce42c3cb4cc74b053623
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-02-04 18:23:02 +0100

    Python Basics - Loops - Triple Greeting

---
+++ PY100/python_basics/3.loops/3.triple_greeting.py
@@ -0,0 +1,10 @@
+# Write a loop that prints the value of the greeting variable three times.
+greeting = 'Aloha!'
+
+for _ in range(3):
+    print(greeting)
+
+count = 1
+while count <= 3:
+    print(greeting)
+    count += 1
