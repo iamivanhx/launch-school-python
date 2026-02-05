69afcdc83a89940e4e4e3cfa7413fd14e6affb2b
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-02-05 20:14:08 +0100

    Python Basics - Variable Scope - What's my value part 2

---
+++ PY100/python_basics/6.variable_scope/2.whats_my_value2.py
@@ -0,0 +1,9 @@
+# What will the following code do and why? Don't run it until you have tried to answer.
+x = 10
+
+def my_function():
+    x = x + 5
+    print(x)
+
+my_function() # UnboundLocalError
+
