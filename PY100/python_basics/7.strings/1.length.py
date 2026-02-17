5b679fcc14948b7cba0d1653581c6d3d48ab43a6
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-02-17 20:55:08 +0100

    Strings: Ex1

---
+++ PY100/python_basics/7.strings/1.length.py
@@ -0,0 +1,4 @@
+# Determine the length of the string "These aren't the droids you're looking for."
+my_string = "These aren't the droids you're looking for."
+
+print(f"The length of string {my_string} is {len(my_string)}")
