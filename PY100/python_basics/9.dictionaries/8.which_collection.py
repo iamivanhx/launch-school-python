0fc342196c35c4e4a9b07aaa522c9f9189a3f643
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-02-23 20:30:51 +0100

    Dict: Which collection?

---
+++ PY100/python_basics/9.dictionaries/8.which_collection.py
@@ -0,0 +1,10 @@
+# Rewrite car as a list of lists in which each nested list contains two elements that represent the corresponding key/value pairs.
+car = {
+    'type':  'sedan',
+    'color': 'blue',
+    'year':  2003,
+}
+
+car_list = [[key, value] for key, value in car.items()]
+
+print(car_list)
