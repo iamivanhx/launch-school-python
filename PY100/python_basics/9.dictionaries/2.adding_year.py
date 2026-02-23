f00e5a7d062c8a8ef88a8901b4a8e445f86d2e14
Ivan E. Hernandez T. <ivan@ivanhx.com>
2026-02-23 20:12:00 +0100

    Dict: Adding a Year

---
+++ PY100/python_basics/9.dictionaries/2.adding_year.py
@@ -0,0 +1,11 @@
+# Add some code below the following code to define a year key with a value of 2003. Don't update the dictionary literal
+
+car = {
+    'type':    'sedan',
+    'color':   'blue',
+    'mileage': 80_000,
+}
+
+car["year"] = 2003
+
+print(car)
